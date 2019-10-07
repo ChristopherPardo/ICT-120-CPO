0d502de8f0cf1f28f3e137b4d0cabc89d88670ff
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2019-10-07 15:55:28 +0200

    ExVrac fini 5,6

--- ExVrac/JS/ExVrac.js
+++ ExVrac/JS/ExVrac.js
@@ -9,7 +9,7 @@ function Ex2() {
 function Ex3Over() {
     div3.classList.add("div3");
 }
-function Ex3OLeave() {
+function Ex3Leave() {
     div3.classList.remove("div3");
 }
 function Ex4() {
@@ -18,14 +18,31 @@ function Ex4() {
         div4.classList.add("div4");
     }
 }
+function Ex5() {
+    document.getElementById("div52").remove();
+}
+function Ex6() {
+    var ini = name6.value.substr(0,1);
+    var pos = name6.value.indexOf(" ");
+    if(pos != -1) {
+        ini += name6.value.substr(pos + 1, 1);
+    }
+    var longueur = name6.value.length;
+    if (name6.value.indexOf(" ") != -1 && name6.value.substr(longueur -1, 1) != name6.value.substr(pos + 1, 1)){
+        ini += name6.value.substr(longueur -1, 1);
+    }
 
+    initiales6.value = ini.toUpperCase();
+}
 
 
 function init() {
     div1.addEventListener("click", Ex1);
     div2.addEventListener("click", Ex2);
     div3.addEventListener("mouseover", Ex3Over);
-    div3.addEventListener("mouseleave", Ex3OLeave)
+    div3.addEventListener("mouseleave", Ex3Leave);
     div4bt1.addEventListener("click", Ex4);
     div4bt2.addEventListener("click", Ex4);
+    div51.addEventListener("click", Ex5)
+    name6.addEventListener("keyup", Ex6)
 }
