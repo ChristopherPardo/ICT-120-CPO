2362d2b6a41b91762dd05ee3e8b1501723354349
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2019-11-10 13:52:36 +0100

    fin de la création de la page

--- ProjetVoyage_Participants/JS/Participants.js
+++ ProjetVoyage_Participants/JS/Participants.js
@@ -6,7 +6,6 @@ Voyage_Participants
 
 document.addEventListener('DOMContentLoaded', init)
 
-var nb_line = 0
 var mailto_link = 'mailto:'
 
 function init(){
@@ -24,9 +23,6 @@ function init(){
   FormLastName.addEventListener("keyup", Initials)
   FormLastName.addEventListener("keyup", Lastname_Mail)
   SelectClass.addEventListener("change", ClassSelection)
-  //Bts_Edit = document.getElementsByClassName("Bt_Edit")
-  //Bts_Edit.addEventListener("click", Edit_Form)
-  //Attente de la réponse de Carrel*/
 }
 
 function Firstname_FirstMaj(){
@@ -54,24 +50,13 @@ function Initials() {
     ini += FormLastName.value.substr(0,1)
     ini += FormLastName.value.substr(FormLastName.value.length -1, 1)
     FormInitials.value = ini.toUpperCase()
-
-    /*var pos = name6.value.indexOf(" ");
-    if(pos != -1) {
-        ini += name6.value.substr(pos + 1, 1);
-    }
-    var longueur = name6.value.length;
-    if (name6.value.indexOf(" ") != -1 && name6.value.substr(longueur -1, 1) != name6.value.substr(pos + 1, 1)){
-        ini += name6.value.substr(longueur -1, 1);
-    }
-
-    initiales6.value = ini.toUpperCase();*/
 }
 
 function ClassSelection(){
   if(SelectClass.value != "All"){
     lines = document.querySelectorAll('tr')
     lines.forEach(function (trh) {
-      if (trh.lastChild.previousSibling.previousSibling.previousSibling.innerText != SelectClass.value){
+      if (trh.lastChild.previousSibling.previousSibling.innerText != SelectClass.value){
         trh.hidden = true
       }
       else {
@@ -132,7 +117,7 @@ function FormCheck(){
 
 lines = document.querySelectorAll('tr')
 lines.forEach(function (trm) {
-  if(trm.lastChild.previousSibling.previousSibling.innerText == FormEmail.value){
+  if(trm.lastChild.previousSibling.innerText == FormEmail.value){
     alert("Cet utilisateur existe déjà")
     InfoTrue = 1
   }
@@ -179,9 +164,6 @@ function FormSave(){
   icone_Intra = document.createElement("span")
   link_Intra = document.createElement("a")
   td_Edit = document.createElement("td")
-  icone_Edit = document.createElement("span")
-  icone_Edit.innerText = "fgbhn"
-  icone_Edit.addEventListener('click', Edit_Form)
 
   inp_Check.type = "checkbox"
   td_LastName.innerText = FormLastName.value
@@ -189,22 +171,12 @@ function FormSave(){
   td_Initials.innerText = FormInitials.value
   td_Class.innerText = FormClass.value
   td_Email.innerText = FormEmail.value
-  link_Intra.text = "☞"
+  link_Intra.text = "Intranet"
   var pos = FormEmail.value.indexOf("@");
   intra_Name = FormEmail.value.substr(0,pos);
   pos = FormEmail.value.indexOf(".")
   intra_Name = intra_Name.replace('.', '_')
   link_Intra.href = "http://intranet.cpnv.ch/etudiants/" + intra_Name//+ FormFirstName.value + "_" + FormLastName.value
-  //icone_Edit.classList.add("glyphicon")
-  //icone_Edit.classList.add("glyphicon-pencil")
-
-  /*td_Email.id = "Mail"
-
-  td_Edit.id = "Edit_" + FormInitials.value
-  td_Edit.classList.add("Bt_Intra")
-
-  td_Edit.id = "Edit_" + FormInitials.value
-  td_Edit.classList.add("Bt_Edit")*/
 
   td_Check.appendChild(inp_Check)
   tr.appendChild(td_Check)
@@ -215,31 +187,10 @@ function FormSave(){
   tr.appendChild(td_Email)
   td_Intra.appendChild(link_Intra)
   tr.appendChild(td_Intra)
-  td_Edit.appendChild(icone_Edit)
-  tr.appendChild(td_Edit)
+  if(FormClass.value != SelectClass.value && SelectClass.value != "All"){
+    tr.hidden = true
+  }
   InfoTable.appendChild(tr)
-}
-/*function FormSave(){
-  tr = document.createElement("tr")
-  a = document.createElement("a")
-  a.text = FormLastName.value
-  a.href = "http://intranet.cpnv.ch/etudiants/" + FormFirstName.value +"_"+ FormLastName.value
-  td1 = document.createElement("td")
-  td1.appendChild(a)
-  td2 = document.createElement("td")
-  td2.innerText = FormFirstName.value
-  td3 = document.createElement("td")
-  chk3 = document.createElement("input")
-  chk3.type = "checkbox"
-  tr.appendChild(td3)
-  td3.appendChild(chk3)
-  tr.appendChild(td1)
-  tr.appendChild(td2)
-  InfoTable.appendChild(tr)
-}*/
-
-function Edit_Form(){
-  alert("test")
 }
 
 function DelPerson() {
@@ -257,7 +208,7 @@ function SendMail(){
   checkboxes = document.querySelectorAll('input[type="checkbox"]')
   checkboxes.forEach(function (chk) {
     if (chk.checked){
-      var email = chk.parentNode.parentNode.lastChild.previousSibling.previousSibling
+      var email = chk.parentNode.parentNode.lastChild.previousSibling
       mailto_link += email.innerText + ";"
       MailTo_Send = 1
 
@@ -265,17 +216,7 @@ function SendMail(){
   })
   if(MailTo_Send == 1){
     window = window.open(mailto_link, 'emailWindow')
-    if (window && window.open && !window.closed){
-        window.close()
-      }
       email = ""
       mailto_link = 'mailto:'
     }
 }
-/*
-button = event.target   // La cible ('target') de l'événement est le bouton (un élément du DOM)
-td = button.parentNode  // td est la cellule dans laquelle le bouton se trouve
-tr = td.parentNode      // tr est la ligne ('row') dans laquelle la cellule se trouve
-table = tr.parentNode   // table est la table dans laquelle la ligne se trouve
-table.removeChild(tr)   // On enlève la ligne de la table
- */
